14df182fb08bd3b2dd877eef8c92aa1fdf720c9e
ped02 <ped02@me.com>
2020-10-20 00:16:40 +0800

    Create Entry.m
    Add: Landing Topic for combine

---
+++ topics/Entry.m
@@ -0,0 +1,28 @@
+mem: cName cEmail wType score fail sRank date
+0 score! 0 fail! 0 sRank! 0 date! 0 wType!
+"a" cName
+
+@: replies pos neg
+pos: Continue Yes Yea Yeah Ready
+neg: No End Not_Ready
+replies: :pos :neg
+
+: rankCalc ( n n "s" "s" -- "s") { score fail sRank wType }
+fail 0 > score 50 < sRank "C" same? or or -> "Fail" |
+score 69 < -> "C" |
+score 84 < -> "B" |
+sRank "B" same? sRank "A" same? or score 85 > wType "Citizen / PR" same? and and -> "A*" |
+otherwise "A" |.
+;
+
+include ./topics/button.m
+include ./topics/dateSelect.m
+include ./topics/introduction.m
+include ./topics/workingExperience.m
+include ./topics/experiences.m
+include ./topics/personalInfo.m
+
+Q: $replies.@pos
+A: Lets get started! I would need more information on you. What is your name?
+K: name
+--
