67772b20e6a28f6de3336ef25ff52382c601aa5a
ped02 <ped02@me.com>
2020-10-20 00:14:58 +0800

    Update introduction.m
    Edit for combine

--- topics/introduction.m
+++ topics/introduction.m
@@ -1,20 +1,19 @@
 \ Introduction chatbot flow
 
-@: replies
-replies: Continue End \ Is handled throughout duration of bot since this is not a room
+ \ Is handled throughout duration of bot since this is not a room
 
-Q: $replies.@replies
-A: $replies :Continue same? % Welcome! I would need more information on you. What is your name?
-A: $startChat :End same? % Byeee
-K: name \ Key to "name" room
---
+\ Q: $replies.@replies
+\ A: $replies :pos same? % Welcome! I would need more information on you. What is your name?
+\ A: $startChat :End same? % Byeee
+\ K: name \ Key to "name" room
+\ --
 
 
 room: name
 
 Q: $yourname
 A: Thank you $yourname. Could you also provide us with your email address?
-K: $back email \ Key to "email" room
+K: $yourname cName! % $back email \ Key to "email" room
 --
 
 end-room
@@ -23,9 +22,9 @@ end-room
 
 room: email
 
-Q: $mail
+Q: $m
 A: Thank you. Before we begin, we would like to tell you more about what we do at Keppel. Would you be keen to find out more? ${ "Yes" Answerbutton } ${ "No" Answerbutton }
-K: $back findoutmore \ Key to "findoutmore" room
+K: $m cEmail! % $back findoutmore \ Key to "findoutmore" room
 --
 
 end-room
@@ -38,10 +37,9 @@ room: findoutmore
 findoutmore: Yes No
 
 Q: $findoutmore.@findoutmore
-A: $findoutmore :Yes same? % Watch the video to learn more about Keppel! ${ "nVoFLM_BDgs" 100 200 (utube) }
-A: $findoutmore :No same? % Great! Lets get started!
-\ K:  Key to workingExperience
-
+A: $findoutmore :Yes same? % Watch the video to learn more about Keppel! ${ "nVoFLM_BDgs" 100 200 (utube) } Do you have authorized working rights in Singapore? ${ "Yes" AnswerButton } ${ "No" AnswerButton } }
+A: $findoutmore :No same? % Great! Lets get started! Do you have authorized working rights in Singapore? ${ "Yes" AnswerButton } ${ "No" AnswerButton }
+K: $back workingExperience
 --
 
 end-room
