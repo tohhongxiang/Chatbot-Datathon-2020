aa0ac67232b22616c2f12b1475be34461ac07862
WAIYARAUNG.17 <WAIYARAUNG.17@ichat.sp.edu.sg>
2020-10-20 17:47:51 +0800

    style change and date verification

--- topics/button.m
+++ topics/button.m
@@ -8,17 +8,16 @@
 : AnswerButton ( "s" -- "s" )
   q{ 
     padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
+    border-radius: 8px;
+    display: block;
     color: #fff;
     background-color: #4fc08d;
     transition: all 0.15s ease;
     box-sizing: border-box;
     border: 1px solid #4fc08d;
     opacity: 0.9;
-    font-size: 24px;
+    font-size: 18px;
+    font-weight: bold;
     box-shadow: 3px 3px 4px 2px rgba(0, 0, 0, 0.1);
     outline: none;
   }q  (button) ;
-
-
--- topics/dateSelect.m
+++ topics/dateSelect.m
@@ -1,13 +1,19 @@
 : dateSelector ( -- "s" )
     q{
         <input type="date" id="startDate">
-        <button onclick="submitDate()">Submit</button>
+        <div onclick="submitDate()" style="padding: 0.75em 2em;border-radius: 8px;background-color: #4fc08d;width: fit-content;">Submit</div>
 
         <script>
+            var startDate = document.getElementById('startDate');
+            startDate.min = new Date().toISOString().split("T")[0];
             submitDate = () => {
-                document.getElementById('question').value = document.getElementById('startDate').value;
-                this.form.submit();
+                document.getElementById('question').value = startDate.value;
+                startDate.parentElement.form.submit();
             }
         </script>
     }q
 ;
+
+: @date ( "s" -- f )
+  [regex] ^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$
+; \@
